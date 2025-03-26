0e58890a16e564655dfa6d4e551d34e780a03b6c
Jose Luis <joseluisjlgd123@gmail.com>
2025-03-25 22:44:37 -0500

    Agregar tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <h1 class="text-4xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
