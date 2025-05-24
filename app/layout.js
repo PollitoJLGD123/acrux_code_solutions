408194ccfbfde97742c46089123e7d49ce7d8e86
Jose Luis <joseluisjlgd123@gmail.com>
2025-05-23 23:57:56 -0500

    Eliminar dependecias de HEROIU

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
-import { Providers } from './providers'; // Importa el Provider
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,13 +27,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
 
-          <Header />
-          <main>{children}</main>
-          <Footer />
-          
-        </Providers>
+        <Header />
+        <main>{children}</main>
+        <Footer />
           
       </body>
     </html>
